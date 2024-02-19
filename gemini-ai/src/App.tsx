9aad3ca769d66e800263a22647989ad246e497d6
Aniket Das <47720941+dasaniket20002@users.noreply.github.com>
2024-02-20 00:09:50 +0530

    api working

--- gemini-ai/src/App.tsx
+++ gemini-ai/src/App.tsx
@@ -1,10 +1,29 @@
 import React from 'react';
+import { GoogleGenerativeAI } from "@google/generative-ai";
+
+const API_KEY: string = process.env.REACT_APP_GEN_AI_API_KEY as string;
 
 function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+    const genAI = new GoogleGenerativeAI(API_KEY);
+
+    async function run() {
+        // For text-only input, use the gemini-pro model
+        const model = genAI.getGenerativeModel({ model: "gemini-pro" });
+
+        const prompt = "Write a story about a magic backpack."
+
+        const result = await model.generateContent(prompt);
+        const response = await result.response;
+        const text = response.text();
+        console.log(text);
+    }
+
+    return (
+        <div className="App">
+            <button onClick={() => { run(); }}>RUN</button>
+        </div>
+    );
 }
 
 export default App;
+
